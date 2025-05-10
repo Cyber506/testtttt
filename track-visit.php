3a8ec45b23b390b647f7bd47f81b2f9c9cdd6ee9
cyber <87538460+Cyber506@users.noreply.github.com>
2025-05-10 17:34:24 +0300

    Update track-visit.php

--- track-visit.php
+++ track-visit.php
@@ -8,7 +8,7 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("ddفشل الاتصال بقاعدة البيانات.");
+    die("ddفشل الاتصال بقاعدة البياkkkkنات.");
 }
 
 $ip = $_SERVER['REMOTE_ADDR'];
